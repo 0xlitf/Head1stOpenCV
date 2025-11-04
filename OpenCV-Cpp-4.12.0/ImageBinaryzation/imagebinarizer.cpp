6bfa06163336cd6ec725b8feb0a727bdb9337771
litengfei <0xlitf@gmail.com>
2025-11-04 13:33:16 +0800

    `

---
+++ OpenCV-Cpp-4.12.0/ImageBinaryzation/imagebinarizer.cpp
@@ -0,0 +1,1 @@
+#include "imagebinarizer.h"
--- OpenCV-Cpp-4.12.0/ImageBinaryzation/main.cpp
+++ OpenCV-Cpp-4.12.0/ImageBinaryzation/main.cpp
@@ -1,11 +1,17 @@
 #include "mainwindow.h"
-
+#include "imagebinarizer.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    // MainWindow w;
+    // w.show();
+
+    ImageBinarizer window;
+    window.setWindowTitle("Qt6 图像二值化工具");
+    window.resize(900, 600);
+    window.show();
+
     return a.exec();
 }
