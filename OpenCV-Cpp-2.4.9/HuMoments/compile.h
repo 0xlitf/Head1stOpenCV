ef9e3ba04e2498937f5da0d4bfaab88c935180c3
litengfei <0xlitf@gmail.com>
2025-12-18 16:36:44 +0800

    `

--- OpenCV-Cpp-2.4.9/HuMoments/compile.h
+++
@@ -1,6 +0,0 @@
-#ifndef COMPILE_INFO_H 
-#define COMPILE_INFO_H 
-const char* BUILD_DATE = "2025-12-18 16:28:38.72"; 
-const char* GIT_BRANCH = "main"; 
-const char* GIT_COMMIT_HASH = "75d80c7"; 
-#endif 
