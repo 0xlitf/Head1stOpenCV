b0a9dec8984c69e9ac1d828f87fb0f71312d468c
litengfei <0xlitf@gmail.com>
2026-01-19 11:09:16 +0800

    `

--- OpenCV-Cpp-2.4.9/DefectDetection/compile.h
+++
@@ -1,6 +0,0 @@
-#ifndef COMPILE_INFO_H 
-#define COMPILE_INFO_H 
-const char* BUILD_DATE = "2026-01-09 11:51:48.93"; 
-const char* GIT_BRANCH = "main"; 
-const char* GIT_COMMIT_HASH = "ef127bf"; 
-#endif 
